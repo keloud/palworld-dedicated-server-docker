cd0a17839e9d36f3dbf29868af699b6c0f793d6c
Keisuke Morimoto <keloud@users.noreply.github.com>
2024-02-04 17:05:50 +0900

    Update entry.sh

--- bullseye/etc/entry.sh
+++ bullseye/etc/entry.sh
@@ -7,7 +7,7 @@ bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" \
     +quit
 
 if [ ! -d ${HOMEDIR}/rcon-cli ]; then
-    # Setup RCON Client
+    echo Setup RCON Client
     curl -OL https://github.com/gorcon/rcon-cli/releases/download/v0.10.3/rcon-0.10.3-amd64_linux.tar.gz
     mkdir -v ${HOMEDIR}/rcon-cli
     tar -xzvf rcon-0.10.3-amd64_linux.tar.gz -C ${HOMEDIR}/rcon-cli --strip-components 1
@@ -17,7 +17,7 @@ sed -i -e "s/address: [a-zA-Z0-9:\"]*/address: "127.0.0.1:${RCON_PORT}"/g" \
     -e "s/password: [a-zA-Z0-9:\"]*/password: ${ADMIN_PASSWORD}/g" "${HOMEDIR}/rcon-cli/rcon.yaml"
 
 if [ ! -s ${STEAMAPPDIR}/Pal/Saved/Config/LinuxServer/PalWorldSettings.ini ]; then
-    # Generate PalWorldSettings.ini
+    echo Generate PalWorldSettings.ini
     timeout --preserve-status 15s ${STEAMAPPDIR}/PalServer.sh 1> /dev/null
     sleep 5
 

49122b9d99afae81679d7011e3a8ecf88785cd8f
Keisuke Morimoto <keloud@users.noreply.github.com>
2024-02-10 02:55:30 +0900

    Update backup.sh

--- bullseye/etc/backup.sh
+++ bullseye/etc/backup.sh
@@ -6,9 +6,8 @@ if [ ! -d ${BACKUPDIR} ]; then
   return 1
 fi
 
-if [ ! -f ${BACKUPDIR}/.latest_backup_date ]; then
+if [ -f ${BACKUPDIR}/.latest_backup_date ]; then
   OLDDATE=$(cat ${BACKUPDIR}/.latest_backup_date)
-  touch ${BACKUPDIR}/.latest_backup_date
 fi
 OLDDATE=${OLDDATE:="NOT_SET"}
 NEWDATE=$(date +%Y%m%d-%H%M)

d097956b9c3895b23de8a1bdbd603d5d6b701add
Keisuke Morimoto <keloud@users.noreply.github.com>
2024-02-10 15:59:50 +0900

    Update setup_rcon.sh

--- bullseye/etc/setup_rcon.sh
+++ bullseye/etc/setup_rcon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 
-echo Setup RCON Client
+# Setup RCON Client
 curl -OL https://github.com/gorcon/rcon-cli/releases/download/v0.10.3/rcon-0.10.3-amd64_linux.tar.gz
 mkdir -v ${HOMEDIR}/rcon-cli
 tar -xzvf rcon-0.10.3-amd64_linux.tar.gz -C ${HOMEDIR}/rcon-cli --strip-components 1

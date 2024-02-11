8556ebda4ad3e41c31a05cf460607158fbe42a9c
Keisuke Morimoto <keloud@users.noreply.github.com>
2024-02-11 15:57:45 +0900

    Update backup.sh

--- bullseye/etc/backup.sh
+++ bullseye/etc/backup.sh
@@ -11,14 +11,17 @@ if [ ! -d ${RCONDIR} ]; then
   exit 1
 fi
 
+# Save
+${RCONDIR}/rcon -c "${RCONDIR}/rcon.yaml" save
+
+# Get latest backup date
 if [ -f ${BACKUPDIR}/.latest_backup_date ]; then
   OLDDATE=$(cat ${BACKUPDIR}/.latest_backup_date)
 fi
 OLDDATE=${OLDDATE:="NOT_SET"}
-NEWDATE=$(date +%Y%m%d-%H%M)
 
-OLDBACKUPDIR=${BACKUPDIR}/${OLDDATE}
-NEWBACKUPDIR=${BACKUPDIR}/${NEWDATE}
+# Get date
+NEWDATE=$(date +%Y%m%d-%H%M)
 
 if [ "${OLDDATE}" = "${NEWDATE}" ]; then
   echo "Already backed up"
